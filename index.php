724e1e7161bcff36aa202b82c406966b1ff65d2b
TahiriAbdou <tahiri.abdou@outlook.com>
2013-07-19 21:31:53 +0000

    1 comment

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Git Project tutorial</title>
+	<style type="text/css">
+	</style>
+</head>
+<body>
+	<div class="container">
+		<h1>Git project tutorial</h1>
+		<?php phpinfo(); ?>
+	</div>
+</body>
+</html>
---
+++ readme.txt
@@ -0,0 +1,5 @@
+----------------------
+Readme file :
+
+this project is about a new technology called git , 
+Git is a totaly Version Control System , it allows you the full controll of all branches of project .........
